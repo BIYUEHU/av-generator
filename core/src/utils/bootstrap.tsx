2eb57f020af64f1e037ef948302794aeba9de22d
Hotaru <biyuehuya@gmail.com>
2025-09-23 12:19:50 +0800

    fix: update remotion render entry

--- core/src/utils/bootstrap.tsx
+++ core/src/utils/bootstrap.tsx
@@ -1,9 +1,13 @@
-import { Composition } from 'remotion'
+import { Composition, registerRoot } from 'remotion'
 import { Template } from '../types/common'
 import { VideoConfig } from '../types/config'
 import { calculateDurationInFrames } from './common'
 
-export function setup(template: Template, config: VideoConfig) {
+export function bootstrap(template: Template, config: VideoConfig): void {
+  registerRoot(() => bootstrap2(template, config))
+}
+
+export function bootstrap2(template: Template, config: VideoConfig) {
   return (
     <Composition
       id={config.render?.title ?? 'MusicVideo'}
